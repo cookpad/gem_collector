d9cdfe4cbe80c3bfa91cb5dbc95d5bc54a4d7b46
Kohei Suzuki <kohei-suzuki@cookpad.com>
2017-03-17 12:59:25 +0900

    Load octokit.yml

--- lib/gem_collector/engine.rb
+++ lib/gem_collector/engine.rb
@@ -4,5 +4,9 @@ module GemCollector
 
     config.autoload_paths << root.join('lib/autoload').to_s
     config.eager_load_paths << root.join('lib/autoload').to_s
+
+    initializer 'gem_collector.octokit' do |app|
+      app.config.octokit = app.config_for(:octokit)
+    end
   end
 end
