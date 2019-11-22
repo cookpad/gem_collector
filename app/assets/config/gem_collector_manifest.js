c019e6a424305d807972abed75ae24c3b7024902
Kohei Suzuki <kohei-suzuki@cookpad.com>
2019-11-22 20:37:59 +0900

    Delete unused mailer and assets

--- app/assets/config/gem_collector_manifest.js
+++
@@ -1,2 +0,0 @@
-//= link_directory ../javascripts/gem_collector .js
-//= link_directory ../stylesheets/gem_collector .css
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,6 +1,9 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'active_job/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
