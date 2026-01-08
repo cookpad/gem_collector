ee413ef9786c713f2921f64c5998ec1c0e6fe9b0
Takuma Ishikawa <nekketsuuu@cookpad.com>
2026-01-08 15:12:04 +0900

    Upgrade rails to 7.0 and run app:update app:update was run on both / and /spec/dummy

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_03_17_021845) do
-
+ActiveRecord::Schema[7.0].define(version: 2017_03_17_021845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +19,8 @@ ActiveRecord::Schema.define(version: 2017_03_17_021845) do
     t.integer "repository_id", null: false
     t.string "full_name", null: false
     t.string "ssh_url", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["site", "repository_id"], name: "idx_repositories", unique: true
   end
 
@@ -30,8 +29,8 @@ ActiveRecord::Schema.define(version: 2017_03_17_021845) do
     t.string "name", null: false
     t.string "version", null: false
     t.string "path", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["repository_id", "path", "name"], name: "idx_repository_gems", unique: true
   end
 
